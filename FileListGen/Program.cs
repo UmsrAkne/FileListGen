9dd698572f581506665bfd69077feefc0c80365f
Akn <sibata486@gmail.com>
2022-11-21 00:29:41 +0900

    init

---
+++ FileListGen/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FileListGen
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter the directory path in the argument.");
+            }
+            else
+            {
+                var path = args[0];
+                if (!Directory.Exists(path))
+                {
+                    return;
+                }
+
+                var paths = Directory.GetFiles(path);
+                foreach (string p in paths)
+                {
+                    Console.WriteLine($"\"{p}\"");
+                }
+            }
+        }
+    }
+}
